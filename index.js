5b071bb913579b9d24c16f6994f974d84b8b9c67
rbarretto <richard.a.barretto@gmail.com>
2018-02-03 02:42:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,57 @@ function whisper(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+<<<<<<< HEAD
 function logShout(string){
 
 }
 function logShout(string){
   console.log(string)
+=======
+function logShout(string) {
+}
+function logShout(string){
+ return (string)
+>>>>>>> a4e7e658df142375fff5ef4e26815dbfd7359e6d
 
 }
 'Hello'.toUpperCase ()
 function logShout(string){
+<<<<<<< HEAD
   console.log string.toUpperCase()
+=======
+  return console.log(string.toUpperCase())
+}
+function logWhisper(string){
+
+}
+function logWhisper(string){
+ return (string)
+}
+'Hello'.toLowerCase ()
+function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase // true
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase // true
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase // false
+
+mixedCase.toUpperCase() === mixedCase // false
+function sayHiToGrandma(string){
+}
+function sayHiToGrandma(string) {
+  if (string === lowercase){
+    return ("I can't hear you!")
+  } else if (string ===uppercase){
+    return ("YES INDEED")
+  }
+>>>>>>> a4e7e658df142375fff5ef4e26815dbfd7359e6d
 }
